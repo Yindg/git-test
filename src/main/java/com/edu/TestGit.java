a88dd2219db088e551c19af92048d27797cf3f96
Yin <15834052825@163.com>
2022-06-14 18:16:45 +0800

    yin 2022年6月14日18:16:32 first commit

---
+++ src/main/java/com/edu/TestGit.java
@@ -0,0 +1,18 @@
+package com.edu;
+
+/**
+ * @ClassName TestGit
+ * @Author Yin
+ * @Date 2022/6/14 18:12
+ * @Description
+ * @Version 1.0
+ **/
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
